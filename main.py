5571a74ed571a4b0d8d8e03d06d0dcd632b69f2e
kimhyunuk95 <105341749+kimhyunuk95@users.noreply.github.com>
2022-08-17 21:15:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ def main():
     with tab2:
         st.write(a[['상호지점명', '거리']])
     with tab3:
-        percentage_df = pd.DataFrame.from_dict(percentage)
+        percentage_df = pd.DataFrame.from_dict(percentage, index=[0])
         st.write(percentage_df)
     
     
